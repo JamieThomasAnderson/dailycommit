20ddd7c478a4711fe2512de3a8d828448f7b6881
Jamie Anderson <n11251514@qut.edu.au>
2024-05-12 06:23:50 +0000

    start date

--- src/app/heatmap.tsx
+++ src/app/heatmap.tsx
@@ -1,17 +1,32 @@
 "use client";
 
-import React from 'react';
+import React, { useState } from 'react';
 import HeatMap from '@uiw/react-heat-map';
 
+const start = new Date();
+start.setFullYear(start.getFullYear() - 1);
+
+
+
 const Demo = () => {
+  const [selected, setSelected] = useState('')
   return (
     <div>
       <HeatMap
-        weekLabels={['', 'Mon', '', 'Wed', '', 'Fri', '']}
-        startDate={new Date('2016/01/01')}
+        width={600}
+        startDate={start}
+        rectRender={(props, data) => {
+          if (selected !== '') {
+            props.opacity = data.date === selected ? 1 : 0.45
+          }
+          return (
+            <rect {...props} onClick={() => {
+              setSelected(data.date === selected ? '' : data.date);
+            }} />
+          );
+        }}
       />
     </div>
   )
 };
-
 export default Demo
