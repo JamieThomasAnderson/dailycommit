d0b6d8309459ff9376a12fcbee024398da422684
Jamie Anderson <n11251514@qut.edu.au>
2024-06-11 19:01:08 +1000

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,18 @@ import CommitTable from "~/app/_components/table";
 import CommitCalendar from "~/app/_components/calendar";
 import Menu from "~/app/_components/menu";
 import Image from "next/image";
+import ProjectHeatmap from "~/app/_components/heatmap";
 
 export const dynamic = "force-dynamic";
 
+
 const ProjectPin = () => {
   return (
-    <div className="border-2 w-96">
-      <h1>Polytree</h1>
-      <p>Research & Notetaking Tool</p>
+    <div className="border border-neutral-700 rounded h-24 w-96">
+      <div className="p-2">
+        <p className="text-sm">Polytree</p>
+        <p className="text-slate-400">Research & Notetaking Tool</p>
+      </div>
     </div>
   )
 }
@@ -32,22 +36,19 @@ export default async function HomePage() {
       </SignedOut>
       
       <div className="pl-4 pt-12">
-        <p className="text-md font-thin">Pinned</p>
-        <div className="grid grid-cols-2 gap-x-12 gap-y-6">
-          <ProjectPin />
-          <ProjectPin />
-          <ProjectPin />
-          <ProjectPin />
-          <ProjectPin />
-          <ProjectPin />
+        <p className="text-md font-thin pb-2">Pinned</p>
+        <div className="flex items-stretch space-x-2">
           <ProjectPin />
           <ProjectPin />
         </div>
+        <div className="flex items-stretch space-x-2 pt-2">
+          <ProjectPin />
+        </div>
       </div>
-      
+      <ProjectHeatmap />
       </div>
 
-      <div className="col-start-3 col-span-2 p-6 mt-6">
+      <div className="col-start-3 col-span-2 p-6 mt-6 lg:w-64">
         <Image 
           src={user?.imageUrl ?? ''}
           width={600}
@@ -59,14 +60,13 @@ export default async function HomePage() {
       <h1 className="text-xl font-semibold pr-4 pt-4">
         {user?.fullName}
       </h1>
-      <h1 className="text-xl pb-4">
+      <h1 className="text-xl pb-4 text-slate-400 text-wrap">
         {user?.username}
       </h1>
 
       <p className="text-sm">
         Bachelor of Information Technology (Computer Science) Student at the Queensland University of Technology (QUT) 
       </p>
-      
       </div>
     </main>
 
